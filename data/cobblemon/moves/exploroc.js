e26b7e8ffbbf7a5f6dd850b144a2b4d17c48fc85
matheo-1712 <perod.matheo@gmail.com>
2026-01-08 14:20:18 +0100

    fix: Update Explo-roc implementation for correct side condition handling

--- data/cobblemon/moves/exploroc.js
+++ data/cobblemon/moves/exploroc.js
@@ -7,17 +7,18 @@
     pp: 5,
     priority: 0,
     flags: { protect: 1, mirror: 1, metronome: 1, noparentalbond: 1, reflectable: 1, mustpressure: 1 },
-    sideCondition: "exploroc",
-    condition: {
-        // this is a side condition
-        onSideStart(side) {
-            this.add("-sidestart", side, "move: Stealth Rock");
-        },
-        onEntryHazard(pokemon) {
-            if (pokemon.hasItem("heavydutyboots"))
-                return;
-            const typeMod = this.clampIntRange(pokemon.runEffectiveness(this.dex.getActiveMove("stealthrock")), -6, 6);
-            this.damage(pokemon.maxhp * Math.pow(2, typeMod) / 8);
+    onAfterHit(target, source, move) {
+        if (!move.hasSheerForce && source.hp) {
+            for (const side of source.side.foeSidesWithConditions()) {
+                side.addSideCondition("stealthrock");
+            }
+        }
+    },
+    onAfterSubDamage(damage, target, source, move) {
+        if (!move.hasSheerForce && source.hp) {
+            for (const side of source.side.foeSidesWithConditions()) {
+                side.addSideCondition("stealthrock");
+            }
         }
     },
     selfdestruct: "always",
