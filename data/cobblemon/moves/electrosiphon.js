b16aabeb2d277514ea026f81ff01e33760021558
matheo-1712 <perod.matheo@gmail.com>
2026-01-08 23:15:08 +0100

    feat: Add Electro Siphon move with localization, implementation, and integration into Empoleon's move set

---
+++ data/cobblemon/moves/electrosiphon.js
@@ -0,0 +1,15 @@
+({
+    accuracy: 90,
+    basePower: 110,
+    category: "Physical",
+    name: "Electro Siphon",
+    pp: 5,
+    priority: 0,
+    flags: {contact: 1, protect: 1, mirror: 1, metronome: 1},
+    boosts: {
+        spa: 2,
+    },
+    target: "normal",
+    type: "Electric",
+    contestType: "Tough",
+})
