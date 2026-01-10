00ec83e00155f78971943a13cf6cb8bc6c52c541
matheo-1712 <perod.matheo@gmail.com>
2026-01-10 17:34:00 +0100

    feat: Add Verdant Bastion move with implementation and integration into Meganium's move set

---
+++ data/cobblemon/moves/verdantbastion.js
@@ -0,0 +1,61 @@
+({
+    accuracy: true,
+    basePower: 0,
+    category: "Status",
+    name: "Verdant Bastion",
+    pp: 10,
+    priority: 4,
+    flags: { noassist: 1, failcopycat: 1 },
+    stallingMove: true,
+    volatileStatus: "spikyshield",
+    onPrepareHit(pokemon) {
+        return !!this.queue.willAct() && this.runEvent("StallMove", pokemon);
+    },
+    onHit(pokemon) {
+        pokemon.addVolatile("stall");
+    },
+    condition: {
+        duration: 1,
+        onStart(target) {
+            this.add("-singleturn", target, "move: Protect");
+        },
+        onTryHitPriority: 3,
+        onTryHit(target, source, move) {
+            if (!move.flags["protect"]) {
+                if (["gmaxoneblow", "gmaxrapidflow"].includes(move.id))
+                    return;
+                if (move.isZ || move.isMax)
+                    target.getMoveHitData(move).zBrokeProtect = true;
+                return;
+            }
+            if (move.smartTarget) {
+                move.smartTarget = false;
+            } else {
+                this.add("-activate", target, "move: Protect");
+            }
+            const lockedmove = source.getVolatile("lockedmove");
+            if (lockedmove) {
+                if (source.volatiles["lockedmove"].duration === 2) {
+                    delete source.volatiles["lockedmove"];
+                }
+            }
+            if (this.checkMoveMakesContact(move, source, target)) {
+                this.add("-activate", source, "move: Verdant Bastion");
+                source.side.addSideCondition("spikes");
+            } else {
+                this.boost({ def: 1 }, this.activeTarget);
+            }
+            return this.NOT_FAIL;
+        },
+        onHit(target, source, move) {
+            if (move.isZOrMaxPowered && this.checkMoveMakesContact(move, source, target)) {
+                this.damage(source.baseMaxhp / 8, source, target);
+            }
+        }
+    },
+    secondary: null,
+    target: "self",
+    type: "Steel",
+    zMove: { boost: { def: 1 } },
+    contestType: "Tough"
+})
