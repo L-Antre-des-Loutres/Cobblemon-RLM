21a384d0635f8d1fa1a96d43c5e83a01ce43e3d1
matheo-1712 <perod.matheo@gmail.com>
2026-02-18 01:54:54 +0100

    fix: Update Dullness Guard ability logic and assign corrected ability number

--- data/cobblemon/abilities/dullnessguard.js
+++ data/cobblemon/abilities/dullnessguard.js
@@ -1,21 +1,22 @@
 ({
     onTryHit(target, source, move) {
-    if (target === source || move.category === "Status" || move.type === "???" || move.id === "struggle")
-        return;
-    if (move.id === "skydrop" && !source.volatiles["skydrop"])
-        return;
-    this.debug("Dullness Guard immunity: " + move.id);
-    if (target.runEffectiveness(move) >= 1) {
-        if (move.smartTarget) {
-            move.smartTarget = false;
-        } else {
-            this.add("-immune", target, "[from] ability: Dullness Guard");
+        if (target === source || move.category === "Status" || move.type === "???" || move.id === "struggle")
+            return;
+        if (move.id === "skydrop" && !source.volatiles["skydrop"])
+            return;
+        this.debug("Dullness Guard immunity: " + move.id);
+        const effectiveness = target.runEffectiveness(move);
+        if (effectiveness >= 0) {
+            if (move.smartTarget) {
+                move.smartTarget = false;
+            } else {
+                this.add("-immune", target, "[from] ability: Dullness Guard");
+            }
+            return null;
         }
-        return null;
-    }
-},
+    },
     flags: { failroleplay: 1, noreceiver: 1, noentrain: 1, failskillswap: 1, breakable: 1 },
     name: "Dullness Guard",
-        rating: 5,
-    num: 17003
+    rating: 5,
+    num: 25
 })
