a289248b703d34df46dde9947f81d5b555689c6c
matheo-1712 <perod.matheo@gmail.com>
2026-07-01 19:54:07 +0200

    fix : Remove useless message

--- data/cobblemon/abilities/verdantbastion.js
+++ data/cobblemon/abilities/verdantbastion.js
@@ -4,7 +4,6 @@
     num: 17010,
     onDamagingHit(damage, target, source, move) {
         if (this.checkMoveMakesContact(move, source, target, true)) {
-            this.add('-activate', target, 'ability: Verdant Bastion');
             this.damage(source.baseMaxhp / 8, source, target);
             source.side.addSideCondition('spikes');
         }
