e22cfcbd2c713be781f6448456f8a206ff6f04a7
matheo-1712 <perod.matheo@gmail.com>
2026-07-27 17:25:41 +0200

    Fix : Wamek Mega work

--- data/rlm_megas/mega_showdown/showdown/held_items/wamekite.js
+++ data/rlm_megas/mega_showdown/showdown/held_items/wamekite.js
@@ -1,18 +1,12 @@
-{
-  name: "Wamekite",
-      spritenum: 999,
-    megaStone: "Wamek-Mega",
-    megaEvolves: ["Wamek"],
-    itemUser: ["Wamek"],
+({
+    name: "Wamekite",
+    spritenum: 620,
+    megaStone: { "Wamek": "Wamek-Mega", "wamek": "wamekmega" },
+    itemUser: ["Wamek", "wamek"],
     onTakeItem(item, source) {
-  const name = source.species.name;
-  const base = source.baseSpecies.baseSpecies;
-  if (name === base) return false;
-  if (item.megaEvolves?.includes(name)) return false;
-  if (item.megaStone === name) return false;
-  return true;
-},
-  num: -999,
-      gen: 1,
-    isNonstandard: "Past",
-}
+        return !item.megaStone?.[source.baseSpecies.baseSpecies];
+    },
+    num: -998,
+    gen: 6,
+    isNonstandard: "Past"
+})
